03108ccfc9dc80a0414eeb8de5fb99e05a6acc40
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 20:23:39 +0200

    cn't

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,41 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory';
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 
 export default function Routes() {
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  });
+  // const store = configureStore({ history });
   return (
-    <Switch>
-      <Route exact path={"/"}>
-        <Home />
-      </Route>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-      {/* Finally, catch all unmatched routes */}
-        <Route>
-            <NotFound />
-        </Route>
+    // <Provider store={store}> From React Redux
+      // <BrowserRouter basename={process.env.PUBLIC_URL}>
+       
+      // </BrowserRouter>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route>
+         <NotFound />
+     </Route>
     </Switch>
+    // </Provider>
   );
-}
+//   return (
+//     <Switch>
+//       <Route exact path={"/"}>
+//         <Home />
+//       </Route>
+//       <Route exact path="/login">
+//         <Login />
+//       </Route>
+//       {/* Finally, catch all unmatched routes */}
+//         <Route>
+//             <NotFound />
+//         </Route>
+//     </Switch>
+//   );
+ }
