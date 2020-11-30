10da41770fe12a7718d836ff3d3fc2d638a1796d
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 14:21:06 +0200

    polishes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import backoffice from './assets/images/backoffice.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={backoffice} className="App-logo" alt="logo" />
+      <p>
+      Welcome to BackOffice
+      </p>
       </header>
     </div>
   );
