a217b3337c43df54b2dc61709d3878bce0710373
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 19:35:08 +0200

    hm?!?!

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
