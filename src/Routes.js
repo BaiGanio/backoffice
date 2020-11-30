1ce3a97ae0a5384a9e2990beca7a585380e57072
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 20:28:14 +0200

    sht

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import createHistory from 'history/createBrowserHistory';
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
