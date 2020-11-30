193cc9b89e77626a9e0bca1bcf3510a807b6c893
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 20:03:18 +0200

    don't give up

--- src/Routes.js
+++ src/Routes.js
@@ -1,31 +1,22 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 
 export default function Routes() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-    <Route render = {({ location }) => (
-     <Switch location = { location }>
-        <Route exact path = '/' component = { Home } />
-        <Route exact path = '/Login/' component = { Login } />
-        <Route component = { NotFound }/>
+    <Switch>
+      <Route exact path={"/"}>
+        <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      {/* Finally, catch all unmatched routes */}
+        <Route>
+            <NotFound />
+        </Route>
     </Switch>
-     )} />
-  </HashRouter>
-    // <Switch>
-    //   <Route exact path={process.env.PUBLIC_URL + '/'}>
-    //     <Home />
-    //   </Route>
-    //   <Route exact path={process.env.PUBLIC_URL + '/login'}>
-    //     <Login />
-    //   </Route>
-    //   {/* Finally, catch all unmatched routes */}
-    //     <Route>
-    //         <NotFound />
-    //     </Route>
-    // </Switch>
   );
 }
