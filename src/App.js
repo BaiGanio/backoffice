11fcd3b47f4ec6463104e23912b2e52cf5d1e01f
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 15:46:34 +0200

    Nothing special

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import backoffice from './assets/images/backoffice.jpg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import PrimarySearchAppBar from './components/PrimarySearchAppBar';
 
 function App() {
   return (
     <div className="App">
+    <PrimarySearchAppBar/>
+
       <header className="App-header">
       <img src={backoffice} className="App-logo" alt="logo" />
       <p>
       Welcome to BackOffice
       </p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       </header>
+
     </div>
   );
 }
