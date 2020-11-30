43f2bf9acad69953121c0e0b3317e3814b03aed1
Lyuben Kikov <exceptionhelper@gmail.com>
2020-11-30 19:48:30 +0200

    really can't do much here

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route exact path={process.env.PUBLIC_URL + '/'}>
-        <Home />
-      </Route>
-      <Route exact path={process.env.PUBLIC_URL + '/login'}>
-        <Login />
-      </Route>
-      {/* Finally, catch all unmatched routes */}
-        <Route>
-            <NotFound />
-        </Route>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Route render = {({ location }) => (
+     <Switch location = { location }>
+        <Route exact path = '/' component = { Home } />
+        <Route exact path = '/Login/' component = { Login } />
+        <Route component = { NotFound }/>
     </Switch>
+     )} />
+  </HashRouter>
+    // <Switch>
+    //   <Route exact path={process.env.PUBLIC_URL + '/'}>
+    //     <Home />
+    //   </Route>
+    //   <Route exact path={process.env.PUBLIC_URL + '/login'}>
+    //     <Login />
+    //   </Route>
+    //   {/* Finally, catch all unmatched routes */}
+    //     <Route>
+    //         <NotFound />
+    //     </Route>
+    // </Switch>
   );
 }
