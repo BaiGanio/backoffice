87dfd731f14a61e0ff800dce97d79382ef2c4631
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-11 00:04:57 +0200

    lets see

--- src/Routes.js
+++ src/Routes.js
@@ -5,36 +5,15 @@ import Login from "./containers/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 
 export default function Routes() {
-  // const history = createHistory({
-  //   basename: process.env.PUBLIC_URL,
-  // });
-  // const store = configureStore({ history });
   return (
-    // <Provider store={store}> From React Redux
-      // <BrowserRouter basename={process.env.PUBLIC_URL}>
-       
-      // </BrowserRouter>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route>
-         <NotFound />
-     </Route>
+
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route>
+          <NotFound />
+      </Route>
     </Switch>
-    // </Provider>
+
   );
-//   return (
-//     <Switch>
-//       <Route exact path={"/"}>
-//         <Home />
-//       </Route>
-//       <Route exact path="/login">
-//         <Login />
-//       </Route>
-//       {/* Finally, catch all unmatched routes */}
-//         <Route>
-//             <NotFound />
-//         </Route>
-//     </Switch>
-//   );
  }
