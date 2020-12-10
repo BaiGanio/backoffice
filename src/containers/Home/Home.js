e970582486a837a89732e72ad0ef0dffdcbaccd4
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-10 23:59:10 +0200

    changes

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="lander">
       <img src={backoffice} className="App-logo" alt="logo" />
         <h1>Welcome to BackOffice</h1>
-        <p className="text-muted">A simple React app</p>
+        <p className="text-muted">A simple React app _!_</p>
       </div>
     </div>
   );
