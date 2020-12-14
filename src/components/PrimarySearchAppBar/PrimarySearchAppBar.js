91cc06f66b912cf522c838d1575bb9a0caf37ec1
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-15 01:24:06 +0200

    ...

--- src/components/PrimarySearchAppBar/PrimarySearchAppBar.js
+++ src/components/PrimarySearchAppBar/PrimarySearchAppBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
