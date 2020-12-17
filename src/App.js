443798ca772d59640579b0739ba72a05994de14d
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-17 17:02:01 +0200

    Stuffs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import Routes from "./Routes";
-import './App.css';
-import PrimarySearchAppBar from './components/PrimarySearchAppBar/PrimarySearchAppBar';
 
-export default function App() {
+import './App.css';
+
+function App() {
   return (
-    <div className="App  py-3">
-      <PrimarySearchAppBar/>
-      <Routes />
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
+export default App;
