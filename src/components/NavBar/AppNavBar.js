3c2a57bdd6c7d0f1be6fb6ba4a2fd721c8b2561d
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-20 00:05:56 +0200

    still curious

--- src/components/NavBar/AppNavBar.js
+++ src/components/NavBar/AppNavBar.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
-import './AppNavBar.css'
-
-import Routes from "../../Routes";
+import Nav from "react-bootstrap/Nav";
 export default function AppNavBar() {
   return (
     <div className="appbar">
-    <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+     <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
           <Navbar.Brand className="font-weight-bold text-muted">
             Scratch
           </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar>
-  <Routes />
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 </div>
   );
 }
