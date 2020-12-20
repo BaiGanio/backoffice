497cf1b8978c0c23792b9f449797d5409e3a8100
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-20 15:29:33 +0200

    Don't want to speak ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from './components/AppNavBar/AppNavBar';
 
 export default function App() {
--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
+import Login from "./containers/Login/Login";
+
 
 export default function Routes() {
 
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/login" component={Login} />
-      <Route path="/signin" component={SignIn} />
+      <Route path="/login" component={Login} />
+      {/* 
       <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/blog" component={Blog} />
