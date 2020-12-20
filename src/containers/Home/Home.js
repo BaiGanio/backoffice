a765bacbe9b47009d993f43f004e49579cf9d239
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-20 15:05:03 +0200

    ...

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Home.css";
 import backoffice from '../../assets/images/backoffice.jpg';
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default function Home() {
   return (
@@ -9,7 +11,18 @@ export default function Home() {
         <img src={backoffice} className="Home-image" alt="BO" />
         <h1>Welcome to BackOffice</h1>
         <p className="text-muted">A simple React app</p>
+        <Button variant="primary">Primary</Button>{' '}
       </div>
+      <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
     </div>
   );
 }
