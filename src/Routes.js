761bfc6e95ba1b79b8711565e1ef99b7f18fbe98
Lyuben Kikov <exceptionhelper@gmail.com>
2020-12-20 18:10:43 +0200

    Good looking app!

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./containers/Dashboard/Dashboard";
-import NotFound from "./parts/NotFound/NotFound";
+import NotFound from "./pages/NotFound/NotFound";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Blog from "./pages/Blog/Blog";
 import Courses from "./pages/Courses/Courses";
 import Characters from "./pages/Characters/Characters";
 import CharactersMUI from "./pages/CharactersMUI/CharactersMUI";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 
 export default function Routes() {
 
@@ -20,7 +21,7 @@ export default function Routes() {
       <Route path="/characters" component={Characters} /> 
       <Route path="/characters-mui" component={CharactersMUI} />
       <Route path="/courses" component={Courses} />
-      {/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+      <Route path="/forgot-password" component={ForgotPassword} />
       <Route><NotFound /></Route>
     </Switch>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -39,6 +39,8 @@ export default function Login() {
           Login
         </Button>
       </Form>
+      <br/>
+      <a href="/forgot-password"> Forgot password? </a>
     </div>
   );
 }
