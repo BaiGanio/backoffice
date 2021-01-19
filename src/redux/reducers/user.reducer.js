c2bf11e07e691bfdf8516a1c33d5665c20d334e2
Симеон Мечков <montishak@gmail.com>
2021-01-20 00:10:12 +0200

    feat: added environment variables in yml file

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -1,7 +1,8 @@
 import { GLOBAL_STORE_NAME, LOGIN_USER, LOGOUT_USER } from "../constants";
 
 export default function userReducer(
-  state = JSON.parse(window.localStorage.getItem(GLOBAL_STORE_NAME)).user || {},
+  state = JSON.parse(window.localStorage.getItem(GLOBAL_STORE_NAME))?.user ||
+    {},
   action
 ) {
   switch (action.type) {
